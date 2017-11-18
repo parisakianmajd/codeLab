5739a9c5b504ede41258ff9fdb3a86fcb6b52808
Parisa Kianmajd <parisa@Parisas-MacBook-Pro.local>
2017-11-18 15:48:59 -0800

    reorganizing codes

---
+++ miscellaneous/fib.py
@@ -0,0 +1,16 @@
+#Generators vs Functions
+
+def fibonacci(n):
+    '''Fibonacci numbers generator'''
+    a, b, counter = 0, 1, 0
+    while True:
+        if (counter > n): return
+        yield a
+        a, b = b, a + b
+        counter += 1
+        
+f = fibonacci(5)
+
+for x in f:
+    print x,
+print
