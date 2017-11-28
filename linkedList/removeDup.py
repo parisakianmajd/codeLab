56154c17f94a64a304730469dfeb3a025201e7b8
Parisa Kianmajd <parisa@Parisas-MacBook-Pro.local>
2017-11-27 22:26:56 -0800

    removed duplicate nodes from a sorted linkedlist

---
+++ linkedList/removeDup.py
@@ -0,0 +1,54 @@
+# Given a sorted linkedlist, remove all duplicates
+class Node:
+    def __init__(self, val=None, next = None):
+        self.val = val
+        self.next= next
+        
+class LinkedList:
+    def __init__(self, head):
+        self.head = head
+
+    def add(self, newNode):
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = newNode
+
+        
+    def __str__(self):
+        out = []
+        if not self.head:
+            return
+        current = self.head
+        while current.next is not None:
+            out.append(current.next.val)
+            current = current.next
+        return ', '.join(map(str,out))
+
+    def removeDup(self):
+        if not self.head:
+            return self.head
+        current = self.head
+        while current.next is not None:
+            while current.val == current.next.val:
+                current.next = current.next.next
+            current = current.next
+        return self
+
+head = Node()
+ll = LinkedList(head)
+ll.add(Node(1))
+ll.add(Node(1))
+ll.add(Node(1))
+ll.add(Node(1))
+ll.add(Node(1))
+ll.add(Node(2))
+ll.add(Node(2))
+ll.add(Node(5))
+ll.add(Node(8))
+ll.add(Node(8))
+ll.add(Node(21))
+print str(ll)
+ll.removeDup()
+print str(ll)
+
