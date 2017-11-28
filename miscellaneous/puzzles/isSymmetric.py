ac3fd343cea1c83f398942ba81c0fde9d53eeb2d
Kianmajd <parisa.kianmajd@intel.com>
2017-11-28 19:10:40 +0000

    check if a given BST is symmetric

---
+++ miscellaneous/puzzles/isSymmetric.py
@@ -0,0 +1,26 @@
+#Given a binary tree check if it's a mirror of itself
+
+class Node:
+    def __init__(self, val, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class BST:
+    def __init__(self, root):
+        self.root = root
+
+    def is_side_symmetric(self, n1, n2):
+        if n1 == n2:
+            return True
+        if n1 == None or n2 == None:
+            return False
+        return n1.val == n2.val and\
+               self.is_side_symmetric(n1.left, n2.right)and\
+               self.is_side_symmetric(n1.right, n2.left)
+    
+    def is_symmetric(self, root):
+        if root is None:
+            return True
+        return self.is_side_symmetric(root.left, root.right)
+        
