020eeb40ce8b46a14fc380a4b56ce7076d562ca1
Kianmajd <parisa.kianmajd@intel.com>
2017-11-29 19:45:16 +0000

    get min value in the stack in constant time

---
+++ miscellaneous/stack_getMin.py
@@ -0,0 +1,51 @@
+# push(x): Push element x onto stack.
+# pop(): Removes the element on top of the stack.
+# top(): Get the top element.
+# getMin(): Retrieve the minimum element in the stack.
+import sys
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+        self.minElem = None
+    def push(self, x):
+        if len(self.stack) == 0:
+            self.stack.append(x)
+            self.minElem = x
+        elif x < self.minElem:
+            self.stack.append(2 * x - self.minElem)
+            self.minElem = x
+        else:
+            self.stack.append(x)
+
+    def pop(self):
+        if len(self.stack) == 0:
+            return -1
+        x = self.stack.pop()
+        if x < self.minElem:
+            self.minElem = 2 * self.minElem - x
+        return x
+
+    def peek(self):
+        if len(self.stack) == 0:
+            return -1
+        x = self.stack[-1]
+        if x < self.minElem:
+            return self.minElem
+        return x
+
+    def getMin(self):
+        return self.minElem
+
+s = Stack()
+s.push(3)
+s.push(5)
+print "min: " + str(s.getMin())
+s.push(2)
+s.push(1)
+print "min: " + str(s.getMin())
+s.pop()
+print "min: " + str(s.getMin())
+s.pop()
+print "peek: " + str(s.peek())
+    
