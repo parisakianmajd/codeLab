47fed46af791b063ee5d81f24692f06f9e201193
Kianmajd <parisa.kianmajd@intel.com>
2017-11-30 02:01:58 +0000

    zigzag traverse of a binary tree

---
+++ tree+graph/tree/zigzag.py
@@ -0,0 +1,52 @@
+#Given a binary tree, return the zigzag level order traversal of its nodes' values.
+#(ie,from left to right, then right to left for the next level and alternate between).
+
+
+class Node:
+    def __init__(self,key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+
+    def zigzagByLayer(zigzagTraverse, currentLayer, leftToRight):
+        if len(currentLayer) == 0:
+            return
+        zigzagTraverseNodes = []
+        zigzagTraverseValues = []
+        for k in xrange(len(currentLayer)):
+            zigzagTraverseNodes.append(currentLayer[k])
+            zigzagTraverseValues.append(currentLayer[k].val)
+        # append the current row   
+        zigzagTraverse.append(zigzagTraverseValues)
+        nextLayer = []
+        leftToRight = not leftToRight
+        while zigzagTraverseNodes:
+            node = zigzagTraverseNodes.pop(-1)
+            if leftToRight:
+                if node.left is not None:
+                    nextLayer.append(node.left)
+                if node.right is not None:
+                    nextLayer.append(node.right)
+            else:
+                if node.right is not None:
+                    nextLayer.append(node.right)
+                if node.left is not None:
+                    nextLayer.append(node.left)
+        zigzagByLayer(zigzagTraverse, nextLayer, leftToRight)
+        
+    def zigzagLevelOrder(A):
+        # do a bfs with two stacks for nodes and the values
+        currentLayer = [A]
+        zigzagTraverse = []
+        zigzagByLayer(zigzagTraverse, currentLayer, True)
+        return zigzagTraverse
+
+root = Node(3)
+root.left = Node(9)
+root.right = Node(20)
+root.right.left = Node(15)
+root.right.right = Node(7)
+
+
+print zigzagLevelOrder(root)
