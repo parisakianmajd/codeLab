6cc5fa867422e7cd29b7e6c818e589ce224dca30
Parisa Kianmajd <parisa@Parisas-MacBook-Pro.local>
2017-12-02 20:15:12 -0800

    converting excel columns to numbers and vice versa

---
+++ miscellaneous/excel_numbers.py
@@ -0,0 +1,21 @@
+#Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+#  1 -> A
+#  26 -> Z
+#  28 -> AB
+
+def numToCol(num):
+    column = ""
+    while num > 0:
+        num, remainder = divmod(num - 1, 26)
+        column = chr(65 + remainder) + column
+    return column
+
+def colToNum(col):
+    num = 0
+    for c in col:
+        num = num * 26 + (ord(c.upper()) - ord('A')) + 1
+    return num
+
+
+print numToCol(28)
+print colToNum('AB')
