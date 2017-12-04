644bbe2880d5e0ca3b695e338690ee2d4e41839a
Parisa Kianmajd <parisa.kianmajd@gmail.com>
2017-12-04 08:19:48 -0800

    commenting

--- array/prefixArr/equilibrium.py
+++ array/prefixArr/equilibrium.py
@@ -1,6 +1,9 @@
 # Equilibrium index of an array is an index such that the sum of elements
 # at lower indexes is equal to the sum of elements at higher indexes.
 
+# solution. 1- Find the total sum of array as the rightsum
+# 2- as moving from left to righ, subtract the current num from the leftsum and add it to the right sum
+
 # Given an array turns an equilibrium index (if any)
 # or -1 if no equilibrium indexes exist.
 
