c4187fc9016c71fe4f248126f6ca99154e8b71c5
Parisa Kianmajd <parisa@Parisas-MacBook-Pro.local>
2017-12-03 21:11:14 -0800

    array

--- array/plusOneArray.py
+++ array/plusOneArray.py
@@ -25,6 +25,26 @@ def inc(A):
         while A[i] == 0:
             i += 1
         A = A[i:]
-        return A                
+        return A
+
+def plusOne(A):
+        # remove leading zeros in input
+        while len(A) > 0 and A[0] == 0:
+            del A[0]
+        # placeholder zero in case most significant digit has a carry
+        A.insert(0,0)
+
+        carry = 1
+        for i in range(len(A) - 1, -1, -1):
+            result = A[i] + carry
+            carry, A[i] = divmod(result, 10)
+    
+        # remove any leading 0
+        if A[0] == 0:
+            del A[0]
+    
+        return A
     
 print inc([ 6, 4, 7, 7, 8, 9 ])
+print plusOne([ 6, 4, 7, 7, 8, 9 ])
+
