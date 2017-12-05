4ba63c61aff209efbc0a8dffcd0c4eaa9bd6cc0f
Parisa Kianmajd <parisa.kianmajd@gmail.com>
2017-12-05 12:53:51 -0800

    Update flip.py

--- array/prefixArr/flip.py
+++ array/prefixArr/flip.py
@@ -7,7 +7,10 @@
 #If there are multiple solutions, return the lexicographically smallest pair of L and R.
 
 # if A is the number of 0s in a suset and B is the number of 1s, the net change in number of 1s after the flip is A - B
+# Transform this problem to maximal segment sum problem by changing 0s to 1s and 1s to -1s
+
 # Use Kadane algorithm to find a subset that maximizes this 
+
 
 
 def flip(A):
