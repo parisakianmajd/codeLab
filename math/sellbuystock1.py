56934dc0e0a83c970a73a38751cbe20de7437218
Parisa Kianmajd <parisa@Parisas-MacBook-Pro.local>
2017-12-07 20:13:46 -0800

    sell and buy stocks to maximize the profit

---
+++ math/sellbuystock1.py
@@ -0,0 +1,32 @@
+# Stock Buy Sell to Maximize Profit
+#1. Find the local minima and store it as starting index. If not exists, return.
+#2. Find the local maxima. and store it as ending index. If we reach the end, set the end as ending index.
+#3. Update the solution (Increment count of buy sell pairs)
+#4. Repeat the above steps if end is not reached.
+
+
+def stockBuySell(prices):
+    n = len(prices)
+    if n == 1:
+        return
+    solutions = []
+    i = 0
+    while i < n-1:
+        while i < n-1 and prices[i+1] <= prices[i]:
+            i += 1
+        if i == n - 1:
+            break
+        buy = i
+        i += 1
+        while i < n and prices[i] > prices[i-1]:
+            i += 1
+        sell = i - 1
+        solutions.append((buy, sell))
+
+    for s in solutions:
+        print "Buy on : %d,  Sell on day: %d" % (s[0], s[1])
+    return
+    
+prices = [100, 180, 260, 310, 40, 535, 695]
+prices = [1,2,1]
+stockBuySell(prices)
