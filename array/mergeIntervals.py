3bb224620ce26745305b34f90e87ceae7a81f180
Parisa Kianmajd <parisa.kianmajd@gmail.com>
2017-12-11 10:25:27 -0800

    changed struct

--- array/mergeIntervals.py
+++ array/mergeIntervals.py
@@ -1,6 +1,12 @@
+
+class Interval:
+     def __init__(self, s=0, e=0):
+         self.start = s
+         self.end = e
+
+
 def mergeIntervals(intervals):
-    # sort the intervals according to the start time
-    sorted_by_start = sorted(intervals, key = lambda tup: tup[0])
+    sorted_by_start = sorted(intervals, key = lambda x: x.start)
     merged = []
 
     for higher in sorted_by_start:
@@ -10,15 +16,20 @@ def mergeIntervals(intervals):
         else:
             lower = merged[-1]
             # if it overlaps with top of stack 
-            if higher[0] <= lower[1]:
-                upper_bound = max(lower[1], higher[1])
-                merged[-1] = (lower[0], upper_bound)  
+            if higher.start <= lower.end:
+                upper_bound = max(lower.end, higher.end)
+                merged[-1] = Interval(lower.start, upper_bound)  
             else:
                 # if it doesn't overlap with top of the stack push it
                 merged.append(higher)
     return merged
 
 
-intervals = [(6,8), (1,9), (2,4), (4,7)]
-intervals2 = [[1,3],[2,6],[8,10],[15,18]]
-print mergeIntervals(intervals2)
+
+times = [(6,8), (1,9), (2,4), (4,7)]
+intervals = []
+for t in times:
+    intervals.append(Interval(t[0],t[1]))
+merged= mergeIntervals(intervals)
+for  m in merged:
+    print (m.start, m.end),
