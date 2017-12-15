cd51ae4bb499407b80c70d2dde8c1abfcfc63ea3
Parisa Kianmajd <parisa.kianmajd@gmail.com>
2017-12-14 16:56:22 -0800

    Update stack_getMin.py

--- miscellaneous/stack_getMin.py
+++ miscellaneous/stack_getMin.py
@@ -37,6 +37,41 @@ class Stack():
     def getMin(self):
         return self.minElem
 
+    
+class MinStack:
+        
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+        
+    def push(self, x):
+        if len(self.stack) == 0:
+            self.minstack.append(x)
+            self.stack.append(x)
+
+        else:
+            self.stack.append(x)
+            if  x < self.minstack[-1]:
+                self.minstack.append(x)  
+            else:
+                self.minstack.append(self.minstack[-1])  
+
+    def pop(self):
+        if len(self.stack) != 0:
+            self.stack.pop()
+            self.minstack.pop()
+
+    def top(self):
+        if len(self.stack) == 0:
+            return -1
+        x = self.stack[-1]
+        return x
+
+    def getMin(self):
+        if len(self.minstack) == 0:
+            return -1
+        return self.minstack[-1]
+
 s = Stack()
 s.push(3)
 s.push(5)
