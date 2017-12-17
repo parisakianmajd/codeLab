bf8cd28b032e4d88b9d28fbf185be6a660fd5dd6
Parisa Kianmajd <parisa@Parisas-MacBook-Pro.local>
2017-12-17 10:08:22 -0800

    two sum

--- hash/twoSum.py
+++ hash/twoSum.py
@@ -1,7 +1,18 @@
-for i in xrange(100):
-    pass
-print i
+# Given an array of integers, find two numbers such that they add up to a specific target number.
 
-for j in xrange(100, -1, -1):
-    pass
-print j
+def twoSum(A, B):        
+    dic = {}
+    for i in range(len(A)):
+        diff = B - A[i]
+        if diff not in dic:
+            if A[i] not in dic:
+                dic[A[i]] = i+1
+        else:
+            return [dic[diff], i+1]
+    
+    return []
+
+A = [ 1, 1, 1 ]
+
+B= 2
+print  twoSum(A, B)
