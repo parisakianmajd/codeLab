24f2ee8a14cb4c90a2f409746790b3e8f1ef5b5f
Parisa Kianmajd <parisa.kianmajd@gmail.com>
2017-12-31 12:08:42 -0800

    Find out if a binary tree is symmetric

---
+++ tree+graph/tree/isSymmetric.py
@@ -0,0 +1,35 @@
+#Given a binary tree check if it's a mirror of itself
+#    1
+#   / \
+#  2   2
+# / \ / \
+#3  4 4  3
+
+class Node:
+    def __init__(self, val, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class BST:
+    def __init__(self, root):
+        self.root = root
+
+    def is_side_symmetric(self, n1, n2):
+        if n1 is None and n2 is None:
+            return True
+        if n1 is None or n2 is None:
+            return False
+        if n1 == n2:
+            return True
+        if n1 == None or n2 == None:
+            return False
+        return n1.val == n2.val and\
+               self.is_side_symmetric(n1.left, n2.right)and\
+               self.is_side_symmetric(n1.right, n2.left)
+    
+    def is_symmetric(self, root):
+        if root is None:
+            return True
+        return self.is_side_symmetric(root.left, root.right)
+        
